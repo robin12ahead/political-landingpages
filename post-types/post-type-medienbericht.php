4fcaf884b4e049ab80e1407b269c5a916798c9e7
robin12ahead <robin@12ahead.ch>
2025-08-22 16:59:50 +0200

    create post type medienbericht

---
+++ post-types/post-type-medienbericht.php
@@ -0,0 +1,96 @@
+<?php
+// proceed if status is set to "active"
+$optionsAddons = get_field('addons', 'option');
+if ($optionsAddons["medienberichte_status"] == true) {
+
+    
+// Add Custom Post Type
+function create_post_type_medienbericht() {
+
+    $supports = array(
+        'title', // post title
+        'editor', // post content
+        'author', // post author
+        'thumbnail', // featured images
+        'excerpt', // post excerpt
+        'custom-fields', // custom fields
+        // 'comments', // post comments
+        'revisions', // post revisions
+        // 'post-formats', // post formats
+    );
+
+    $labels = array(
+        'name' => _x('Medienberichte', 'plural'),
+        'singular_name' => _x('Medienbericht', 'singular'),
+        'menu_name' => _x('Medienberichte', 'admin menu'),
+        'name_admin_bar' => _x('Medienberichte', 'admin bar'),
+        'add_new' => _x('Neuer Medienbericht', 'add new'),
+        'add_new_item' => __('Neuer Medienbericht'),
+        'new_item' => __('Neuer Medienbericht'),
+        'edit_item' => __('Medienbericht bearbeiten'),
+        'view_item' => __('Medienbericht ansehen'),
+        'all_items' => __('Alle Medienberichte'),
+        'search_items' => __('Medienberichte suchen'),
+        'not_found' => __('keine Medienberichte gefunden'),
+    );
+
+    $args = array(
+        'menu_icon' => 'dashicons-document',
+        'supports' => $supports,
+        'labels' => $labels,
+        'public' => false,
+        'show_ui' => true,
+        'publicity_querable' => true,
+        'query_var' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'show_in_nav_menus' => false,
+        'rewrite' => array('slug' => 'medienbericht'),
+        // 'rewrite' => false,
+        'hierarchical' => false,
+    );
+
+    register_post_type('medienbericht', $args);
+}
+add_action('init', 'create_post_type_medienbericht', 5);
+
+// Add Custom Tag Taxonomy
+function register_medienbericht_category() {
+
+    $labels = array(
+        'name'                       => __( 'Kategorien', 'political-landingpages' ),
+        'singular_name'              => __( 'Kategorie', 'political-landingpages' ),
+        'menu_name'                  => __( 'Kategorien', 'political-landingpages' ),
+        'edit_item'                  => __( 'Kategorie Bearbeiten', 'political-landingpages' ),
+        'update_item'                => __( 'Kategorie aktualisieren', 'political-landingpages' ),
+        'add_new_item'               => __( 'Neue Kategorie', 'political-landingpages' ),
+        'new_item_name'              => __( 'Neue Kategorie', 'political-landingpages' ),
+        'parent_item'                => __( 'Übergeordnete Kategorie', 'political-landingpages' ),
+        'parent_item_colon'          => __( 'Übergeordnete Kategorie:', 'political-landingpages' ),
+        'all_items'                  => __( 'Alle Kategorien', 'political-landingpages' ),
+        'search_items'               => __( 'Kategorien suchen', 'political-landingpages' ),
+        'popular_items'              => __( 'Populäre Kategorien', 'political-landingpages' ),
+        'separate_items_with_commas' => __( 'Separate categories with commas', 'political-landingpages' ),
+        'add_or_remove_items'        => __( 'Add or remove tags', 'political-landingpages' ),
+        'choose_from_most_used'      => __( 'Choose from the most used tags', 'political-landingpages' ),
+        'not_found'                  => __( 'No tags found.', 'political-landingpages' ),
+    );
+
+    $args = array(
+        'labels'            => $labels,
+        'public'            => true,
+        'show_in_nav_menus' => true,
+        'show_ui'           => true,
+        'show_tagcloud'     => true,
+        'hierarchical'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+
+    );
+
+    register_taxonomy( 'medienbericht_category', array( 'medienbericht' ), $args );
+
+}
+
+add_action( 'init', 'register_medienbericht_category', 0 );
+}

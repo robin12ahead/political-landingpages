26282282bea8b8b0563cec9bf2ee50337c837a6d
robin12ahead <robin@12ahead.ch>
2025-09-03 16:09:17 +0200

    update soMe shortcode

--- shortcodes/shortcode-social-media-links.php
+++ shortcodes/shortcode-social-media-links.php
@@ -17,28 +17,37 @@ function social_media_shortcode( $atts ) {
 
                 $output .= '<a href="' . $link . '" class="social-media-link" target="_blank">';
 
-                    // Handle if the return type is an array.
+                    // Handle if the return type is a string.
+                    if ( is_string( $icon ) ) {
 
-                    // If the type selected was a Dashicon, render a div with the dashicon class.
-                    if ( 'dashicons' === $icon['type'] ) {
-                        $output .= '<div class="' . esc_attr( $icon['value'] ) . '"></div>';
+                        // If the type selected was a Dashicon, the value of $icon will be the dashicon class string.
+                        // If the type selected was a Media Library image, the value of $icon will be the URL to the image.
+                        // If the type selected was a URL, the value of $icon will be the URL to the image.
+                        $output .= esc_html( $icon );
+
+                    } else {
+                        // Handle if the return type is an array.
+
+                        // If the type selected was a Dashicon, render a div with the dashicon class.
+                        if ( 'dashicons' === $icon['type'] ) {
+                            $output .= '<div class="' . esc_attr( $icon['value'] ) . ' dashicons"></div>';
+                        }
+
+                        // If the type selected was a Media Library image, use the attachment ID to get and render the image.
+                        if ( 'media_library' === $icon['type'] ) {
+                            $attachment_id = $icon['value'];
+                            $size = 'full'; // (thumbnail, medium, large, full, or custom size)
+
+                            $image_html = wp_get_attachment_image( $attachment_id, $size );
+                            $output .= wp_kses_post( $image_html );
+                        }
+
+                        // If the type selected was a URL, render an image tag with the URL.
+                        if ( 'url' === $icon['type'] ) {
+                            $url = $icon['value'];
+                            $output .= '<img src="' . esc_url( $url ) .'" alt="">';
+                        }
                     }
-
-                    // If the type selected was a Media Library image, use the attachment ID to get and render the image.
-                    if ( 'media_library' === $icon['type'] ) {
-                        $attachment_id = $icon['value'];
-                        $size = 'full'; // (thumbnail, medium, large, full, or custom size)
-
-                        $image_html = wp_get_attachment_image( $attachment_id, $size );
-                        $output .= wp_kses_post( $image_html );
-                    }
-
-                    // If the type selected was a URL, render an image tag with the URL.
-                    if ( 'url' === $icon['type'] ) {
-                        $url = $icon['value'];
-                        $output .= '<img src="' . esc_url( $url ) .'" alt="">';
-                    }
-
                         
                 $output .= '</a>';
             }

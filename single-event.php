56edbf4042bcaf445f3811bcf93f2b4507c3ad60
robin12ahead <robin@12ahead.ch>
2025-09-08 17:22:20 +0200

    update events

--- single-event.php
+++ single-event.php
@@ -105,6 +105,10 @@ $overviewPageUrl = get_permalink( get_page_by_path( 'events' ) );
 					the_post();
 
 					get_template_part( 'template-parts/content', get_post_type() ); 
+
+                    if (get_field("event_link")) : ?>
+                        <a class="button btn btn-has-arrow" href="<?php get_field("event_link"); ?>">
+                    <?php endif;
 					
 					the_post_navigation(
 						array(

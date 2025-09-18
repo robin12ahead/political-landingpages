70f64661a3b31f9892022c91e78938422d1691d6
robin12ahead <robin@12ahead.ch>
2025-09-18 14:29:05 +0200

    test autodeploy

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="site">
+<div id="page" class="site test-auto-deploy">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'political-landingpages' ); ?></a>
 
 	<header id="site-header" class="header">
